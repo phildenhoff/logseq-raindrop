15a6d31b29d0e955aed294459bc44e449c1ea4d7
Phil Denhoff <phil@denhoff.ca>
2022-10-06 22:06:33 -0700

    make sure each tag is separate from the others

--- src/util/pageFormatter.ts
+++ src/util/pageFormatter.ts
@@ -7,6 +7,6 @@ export const formatRaindropToProperties = (
     "raindrop-id": r.id,
     "raindrop-title": r.title,
     "raindrop-url": r.url.toString(),
-    "tags": r.tags.join(" "),
+    "tags": r.tags.map(tag => `[[${tag}]]`).join(" "),
   };
 };

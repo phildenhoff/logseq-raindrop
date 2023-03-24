ef0b43a9518db5c55e11e5d8156ae883d8c2147e
Phil Denhoff <phil@denhoff.ca>
2023-03-24 16:58:29 -0700

    feat: users can set default page tags

--- src/util/pageFormatter.ts
+++ src/util/pageFormatter.ts
@@ -1,12 +1,27 @@
 import type { Raindrop } from "@types";
 
+type DefaultProperties = {
+  tags: string;
+};
+
+/**
+ * Returns a string of tags to be added to the page
+ * @param tagList The list of tags from Raindrop
+ * @param defaultTags The user's default tags. This can be an empty string.
+ */
+const generateTags = (tagList: Raindrop["tags"], defaultTags: string) => {
+  const separator = defaultTags.length > 0 ? " " : "";
+  return defaultTags + separator + tagList.map((tag) => `[[${tag}]]`).join(" ");
+};
+
 export const formatRaindropToProperties = (
-  r: Raindrop
+  r: Raindrop,
+  defaults?: DefaultProperties | undefined
 ): Record<string, string> => {
   return {
     "raindrop-id": r.id,
     "raindrop-title": r.title,
     "raindrop-url": r.url.toString(),
-    tags: r.tags.map((tag) => `[[${tag}]]`).join(" "),
+    tags: generateTags(r.tags, defaults?.tags ?? ""),
   };
 };

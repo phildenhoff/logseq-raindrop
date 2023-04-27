3f754f4af4b56faf5b7f7a7a9d30e7de3dcba754
Phil Denhoff <phil@denhoff.ca>
2023-04-27 01:25:28 -0700

    feat: list bookmarks since lastSyncTimestampValue (1pg)

--- src/components/organisms/PageImport.svelte
+++ src/components/organisms/PageImport.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-  import { settings } from "@util/settings.js";
   import { derived, writable } from "svelte/store";
 
-  let lastSyncDatetime = writable(0);
+  import type {Raindrop as TRaindrop} from "@types";
+  import { settings } from "@util/settings.js";
+  import { raindropTransformer } from "@util/raindropTransformer.js";
+  import Raindrop from "@atoms/Raindrop.svelte";
+
+  let lastSyncTimestampValueStr = writable('0');
+  const lastSyncTimestampSecs = derived(
+    lastSyncTimestampValueStr,
+    ($value) => Number($value) || 0
+  );
 
   const hasEnabledExperimentalFeatures = settings.enable_broken_experimental_features(); 
 
-  const msToSeconds = (value: number) => value * 1000;
+  const secondsToMs = (value: number) => value * 1000;
   const formatterOptions = {
     year: "numeric",
     month: "numeric",
@@ -14,18 +22,80 @@
     hour: "numeric",
     minute: "numeric",
   } as const;
-  const formatMsAsDateTime = (value: number) => Intl.DateTimeFormat(undefined, formatterOptions).format(msToSeconds(value));
+  const formatSecondsAsDateTime = (seconds: number) => Intl.DateTimeFormat(undefined, formatterOptions).format(secondsToMs(seconds));
+
+
+  const remoteData = writable<TRaindrop[]>([]);
+  const requestsInFlight = writable(0);
+  const mostRecentRequestTime = writable(new Date(0));
+  const loading = derived(
+    requestsInFlight,
+    ($requestsInFlight) => $requestsInFlight > 0
+  );
+  const sinceLastUpdate = derived(
+    [remoteData, lastSyncTimestampSecs],
+    ([$remoteData2, $lastSyncDatetime]) => {
+      const lastSyncDate = new Date($lastSyncDatetime * 1000);
+      return $remoteData2.filter((raindrop) => raindrop.created > lastSyncDate);
+    }
+  )
+
+
+  const performSearch = async (): Promise<void> => {
+    const requestTime = new Date();
+    requestsInFlight.update((n) => n + 1);
+
+    const res = await fetch(
+      `https://api.raindrop.io/rest/v1/raindrops/0?sort=-created&perpage=40&version=2`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Authorization: `Bearer ${settings.access_token()}`,
+          "Content-Type": "application/json",
+        }),
+      }
+    );
+    const { items } = await res.json();
+
+    requestsInFlight.update((n) => n - 1);
+    mostRecentRequestTime.update((currentRequestTime) => {
+      if (requestTime < currentRequestTime) return currentRequestTime;
+      remoteData.update((_) => items.map(raindropTransformer));
+      return requestTime;
+    });
+  };
+  const onSearch = (): void => {
+    performSearch();
+  };
 </script>
 
 {#if hasEnabledExperimentalFeatures}
 <div class="experimental">
   <h3>Import Recently Added</h3>
   <p>Bookmarks new to your Raindrop. This does not include old bookmarks that you've recently updated by adding new tags or annotations.</p>
-  <button>Fetch new bookmarks</button>
+  <button on:click={onSearch}>Fetch new bookmarks</button>
   <form class="sync-container">
-    <label for="last-sync">Last sync: {formatMsAsDateTime($lastSyncDatetime)}</label>
-    <input id="last-sync" type="text" bind:value={$lastSyncDatetime}/>
+    <label for="last-sync">Last sync: {formatSecondsAsDateTime($lastSyncTimestampSecs)}</label>
+    <input id="last-sync" type="text" bind:value={$lastSyncTimestampValueStr}/>
   </form>
+
+  <div>
+    <h4>Raindrops since last sync</h4>
+    {#each $sinceLastUpdate as result}
+      <Raindrop 
+        full={false}
+        title={result.title}
+        description={result?.description}
+        annotations={result?.annotations}
+        tags={result?.tags}
+        url={result?.url}
+        created={result?.created}
+        collectionName={result?.collectionName}
+        coverImage={result?.coverImage}
+        onClick={() => console.log("ok")}
+      />
+    {/each}
+  </div>
 </div>
 {/if}
 

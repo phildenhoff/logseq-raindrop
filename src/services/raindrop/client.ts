78ddaa9f4fcdf66c8d150151f6b531d77cb16d3e
Phil Denhoff <phil@denhoff.ca>
2023-05-26 17:26:27 -0700

    feat: setup Raindrop client

---
+++ src/services/raindrop/client.ts
@@ -0,0 +1,45 @@
+import { raindropTransformer } from "@util/raindropTransformer.js";
+import { just, nothing } from "true-myth/maybe";
+
+import type { RaindropClient } from "./interfaces.js";
+import { generateHttpClient } from "./http.js";
+
+const DEFAULT_BASE_URL = "https://api.raindrop.io/";
+
+const generateRaindropClient = (options: {
+  accessToken: string;
+  baseUrl?: string;
+  apiVersion?: "v1";
+}): RaindropClient => {
+  const apiUrl = `${options.baseUrl ?? DEFAULT_BASE_URL}/rest/${
+    options.apiVersion
+  }`;
+  const httpClient = generateHttpClient({
+    apiUrl,
+    accessToken: options.accessToken,
+  });
+
+  return {
+    getRaindrop: async (id) => {
+      try {
+        const res = await httpClient.getRaindrop(id);
+        if (res.status !== 200) {
+          throw new Error("Request not successful");
+        }
+
+        const resJson = (await res.json()) as unknown;
+        if (typeof resJson !== "object" || !resJson || !("item" in resJson)) {
+          throw new Error("Invalid response");
+        }
+
+        const transformed = raindropTransformer(resJson.item);
+        return just(transformed);
+      } catch {
+        return nothing();
+      }
+    },
+    searchTerm: async (query, collectionId = "0") => {
+      return await httpClient.search(query, collectionId);
+    },
+  };
+};

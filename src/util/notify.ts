6a2edc4597ed14291ff844c7b0f3fbd4e2da5742
Phil Denhoff <phil@denhoff.ca>
2023-05-27 15:05:30 -0700

    feat: prefer using logseq service client

--- src/util/notify.ts
+++ src/util/notify.ts
@@ -1,9 +1,10 @@
 import type { Raindrop } from "@types";
-
-const notify = logseq.UI.showMsg;
-
-export const alertDuplicatePageIdUsed = (r: Raindrop) => {
-  notify(
+import type { LogseqServiceClient } from "src/services/interfaces.js";
+export const alertDuplicatePageIdUsed = (
+  r: Raindrop,
+  client: LogseqServiceClient
+) => {
+  client.displayMessage(
     `You have duplicate pages for this article: \n${r.title}.\n\nWe'll use the oldest one, but you should merge these pages.`,
     "warning",
     { key: r.id, timeout: 5000 }

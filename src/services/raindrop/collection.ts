6c96f560ed63787cbb6668add92c2660ebcdc051
Phil Denhoff <phil@denhoff.ca>
2023-05-30 12:59:45 -0700

    feat: raindrop client can retrieve raindrops updated since

--- src/services/raindrop/collection.ts
+++ src/services/raindrop/collection.ts
@@ -7,3 +7,18 @@ export const searchTerm = async (query: string, collectionId: string = "0") => {
 
   return httpClient.get(`/raindrops/${collectionId}?search=${query}`);
 };
+
+export async function* getCollectionUpdatedSince(
+  since: Date,
+  collectionId: string = "0",
+  params?: {
+    perPage?: number;
+  }
+) {
+  let pageOffset = 0;
+  yield httpClient.get(
+    `/raindrops/${collectionId}?sort-lastUpdate&perpage=${
+      params?.perPage ?? 10
+    }&page=${pageOffset}`
+  );
+}

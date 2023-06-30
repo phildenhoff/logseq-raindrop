79931a3b4a5d8eb21de692ee16f5e6876186f30e
Phil Denhoff <phil@denhoff.ca>
2023-06-30 14:58:03 -0700

    render mustache templates (WIP)

--- src/bookmarks/rendering.ts
+++ src/bookmarks/rendering.ts
@@ -1,22 +1,66 @@
+import Mustache from "mustache";
+
 import type { AppUserConfigs } from "@logseq/libs/dist/LSPlugin.user.js";
 import { formatDateUserPreference } from "@services/logseq/formatting.js";
 import type { Annotation, Raindrop } from "@types";
 
+type HighlightView = {
+  text: string;
+  note: string;
+};
+type BookmarkView = {
+  title: string;
+  url: string;
+  tags: string;
+  dateCreated: string;
+  dateUpdated: string;
+};
+
+const defaultHighlightTemplate = `> {{{text}}}
+
+{{{note}}}`;
+const defaultBookmarkTemplate = `[{{{title}}}]({{{url}}})
+title:: {{{title}}}
+url:: {{{url}}}
+Tags:: {{{tags}}}
+date-saved:: [[{{{dateCreated}}}]]
+last-updated:: [[{{{dateUpdated})}]]`;
+
+export const createHighlightRenderView = (
+  highlight: Annotation
+): HighlightView => {
+  return {
+    text: highlight.text,
+    note: highlight.note,
+  };
+};
+
+export const createBookmarkRenderView = (
+  bookmark: Raindrop,
+  userConfig: AppUserConfigs
+): BookmarkView => {
+  return {
+    title: bookmark.title,
+    url: bookmark.url.toString(),
+    tags: bookmark.tags.join(", "),
+    dateCreated: formatDateUserPreference(bookmark.created, userConfig),
+    dateUpdated: formatDateUserPreference(bookmark.lastUpdate, userConfig),
+  };
+};
+
 export const renderHighlight = (highlight: Annotation) => {
-  return [`> ${highlight.text}`, "", `${highlight.note}`].join("\n");
+  return Mustache.render(
+    defaultHighlightTemplate,
+    createHighlightRenderView(highlight)
+  );
 };
 
 export const renderBookmark = (
   bookmark: Raindrop,
   userConfig: AppUserConfigs
 ) => {
-  return `[${bookmark.title}](${bookmark.url})
-title:: ${bookmark.title}
-url:: ${bookmark.url}
-Tags:: ${bookmark.tags.join(", ")}
-date-saved:: [[${formatDateUserPreference(bookmark.created, userConfig)}]]
-last-updated:: [[${formatDateUserPreference(
-    bookmark.lastUpdate,
-    userConfig
-  )}]]`;
+  return Mustache.render(
+    defaultBookmarkTemplate,
+    createBookmarkRenderView(bookmark, userConfig)
+  );
 };

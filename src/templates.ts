70a006b8488d859407d65505f0c7be0b53e72279
Phil Denhoff <phil@denhoff.ca>
2023-08-23 11:43:36 -0700

    feat: Add templates for markdown & org mode

---
+++ src/templates.ts
@@ -0,0 +1,45 @@
+export type Template = {
+  markdown: string;
+  org: string;
+};
+
+export const defaultHighlightTemplate: Template = {
+  markdown: `> {{{text}}}
+
+  {{{note}}}`,
+  org: `#+BEGIN_QUOTE
+  {{text}}
+  #+END_QUOTE
+
+  {{note}}`,
+};
+
+export const defaultBookmarkTemplate: Template = {
+  markdown: `[{{{title}}}]({{{url}}})
+title:: {{{title}}}
+url:: {{{url}}}
+Tags:: {{{tags}}}
+date-saved:: [[{{{dateCreated}}}]]
+`,
+  org: `[[{{{url}}}][{{{title}}}]]
+:PROPERTIES:
+:title: {{{title}}}
+:url: {{{url}}}
+:Tags: {{{tags}}}
+:date-saved: [[{{{dateCreated}}}]]
+:END:
+`,
+};
+
+export const defaultSavedToRaindropTemplate: Template = {
+  markdown: `Saved to Raindrop: 
+{{{#links}}}
+{{{.}}}
+{{{/links}}}
+`,
+  org: `Saved to Raindrop:
+{{{#links}}}
+{{{.}}}
+{{{/links}}}
+`,
+};
